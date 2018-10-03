4bff4058b3bbb71dec5a48517e62172fa5fc3b2d
Solis Contreras, R <r.solis.contreras@accenture.com>
2018-10-03 11:25:26 -0500

    updated jenkinsfile and responder

---
+++ caller/jenkinsfile.groovy
@@ -0,0 +1,22 @@
+def gitUser = ''
+
+node {
+	stage ('Clone second git repository'){
+		try {
+			repoclone()
+			def responder
+			println responder
+			responder = load 'responder.groovy'
+			println responder
+			responder.hello('hello')
+		} catch (Exception err){
+			println err
+		}
+	}
+}
+
+def repoclone(){
+	checkout([$class: 'GitSCM', branches: [[name: '*/'+'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory'], [$class: 'MessageExclusion', excludeMessage: '(?s).*JENKINS_IGNORE.*']], submoduleCfg: [], userRemoteConfigs: [[url:"https://github.com/ChioSoCo/WorkingWithGroovyFiles.git"]]])
+	}
+}
+
---
+++ responder/responder.groovy
@@ -0,0 +1,9 @@
+def hello(greet){
+	if (greet == "hello"){
+		println 'Hello grom the other side'
+	} else {
+		println 'wzup'
+	}
+}
+return this;
+
