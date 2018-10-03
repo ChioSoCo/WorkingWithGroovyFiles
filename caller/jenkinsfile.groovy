367d5607b10cd36656bcffa401b7238663b6ba9f
ChioSoCo <rociosolis092@gmail.com>
2018-10-03 14:44:00 -0500

    updating close keys

--- caller/jenkinsfile.groovy
+++ caller/jenkinsfile.groovy
@@ -18,5 +18,3 @@ node {
 def repoclone(){
 	checkout([$class: 'GitSCM', branches: [[name: '*/'+'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory'], [$class: 'MessageExclusion', excludeMessage: '(?s).*JENKINS_IGNORE.*']], submoduleCfg: [], userRemoteConfigs: [[url:"https://github.com/ChioSoCo/WorkingWithGroovyFiles.git"]]])
 	}
-}
-
