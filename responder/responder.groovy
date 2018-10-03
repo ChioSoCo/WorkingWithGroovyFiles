758588a5bd90c8a1ea26c14c3afc6d70340048bf
ChioSoCo <rociosolis092@gmail.com>
2018-10-03 15:17:55 -0500

    Adding comments

--- responder/responder.groovy
+++ responder/responder.groovy
@@ -1,9 +1,9 @@
-def hello(greet){
-	if (greet == "hello"){
+def hello(greet){ //defining a function called "hello" which receives an argument and stores it in the "greet" variable.
+	if (greet == "hello"){ //if the greet value iquals "hello" then executes the println "Hello from responder file!"
 		println 'Hello from responder file!'
-	} else {
+	} else { // if the greet values is diffrent to "hello" then executes the println "wzup"
 		println 'wzup'
 	}
 }
-return this;
+//return this; //returs the output of the execution of the function
 
