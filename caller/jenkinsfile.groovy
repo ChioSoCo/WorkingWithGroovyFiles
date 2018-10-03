b79a1d328405d8f2a8e964b08a539912d1f0d901
ChioSoCo <rociosolis092@gmail.com>
2018-10-03 15:28:09 -0500

    Update jenkinsfile.groovy

--- caller/jenkinsfile.groovy
+++ caller/jenkinsfile.groovy
@@ -1,18 +1,15 @@
-node {
-	stage ('Clone second git repository'){
-		try {
-			repoclone()
-			def responder
-			println responder
-			responder = load 'responder/responder.groovy'
-			println responder
-			responder.hello('hello')
-		} catch (Exception err){
+node { //allocates a Jenkins executor and the workspace for the Pipeline
+	stage ('Clone second git repository'){ //Define a stage/step of the pipeline called "Clone second git repository"
+		try { //using try/catch structure to get the errors
+			repoclone() //executes the repoclone function
+			def responder //define a variable 
+			println responder //prints the variable			responder.hello('hello')
+		} catch (Exception err){ //using try/catch structure to get the errors
 			println err
 		}
 	}
 }
 
-def repoclone(){
+def repoclone(){ //defines a function called "repoclone"
 	checkout([$class: 'GitSCM', branches: [[name: '*/'+'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory'], [$class: 'MessageExclusion', excludeMessage: '(?s).*JENKINS_IGNORE.*']], submoduleCfg: [], userRemoteConfigs: [[url:"https://github.com/ChioSoCo/WorkingWithGroovyFiles.git"]]])
 	}
